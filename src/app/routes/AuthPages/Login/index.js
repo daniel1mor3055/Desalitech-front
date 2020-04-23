42034a7e4d195ba2be35b68f114dea59056bb61b
Daniel Mor <dmor@infinidat.com>
2020-04-23 16:15:59 +0300

    changed to login2 instead

--- src/app/routes/AuthPages/Login/index.js
+++ src/app/routes/AuthPages/Login/index.js
@@ -1,51 +1,86 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
+import TextField from '@material-ui/core/TextField';
+import Checkbox from '@material-ui/core/Checkbox';
+import Button from '@material-ui/core/Button';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 import IntlMessages from 'util/IntlMessages';
 
-const Login1 = () => {
-    return (
-        <div
-            className="login-container d-flex justify-content-center align-items-center animated slideInUpTiny animation-duration-3">
-            <div className="login-content">
-                <div className="login-header">
-                    <Link className="app-logo" to="/" title="Desalitech">
-                        <img src={require("assets/images/desalitech-logo-color.png")} alt="desalitech" title="desalitech"/>
-                    </Link>
-                </div>
+class Login extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            name: '',
+            email: '',
+            password: ''
+        }
+    }
 
-                <div className="login-form">
-                    <form>
-                        <fieldset>
-                            <div className="form-group">
-                                <input name="email" id="email" className="form-control form-control-lg"
-                                       placeholder="Email" type="email"/>
-                            </div>
-                            <div className="form-group">
-                                <input name="password" id="password" className="form-control form-control-lg"
-                                       placeholder="Password" type="password"/>
-                            </div>
-                            <div className="form-group d-flex justify-content-between align-items-center">
-                                <label className="custom-control custom-checkbox float-left">
-                                    <input type="checkbox" className="custom-control-input"/>
-                                    <span className="custom-control-indicator"/>
-                                    <span className="custom-control-description">Remember me</span>
-                                </label>
+    render() {
+        const {
+            email,
+            password
+        } = this.state;
+        return (
+            <div
+                className="login-container d-flex justify-content-center align-items-center animated slideInUpTiny animation-duration-3">
+                <div className="login-content">
+                    <div className="login-header mb-4">
+                        <Link className="app-logo" to="/" title="Desalitech">
+                            <img src={require("assets/images/desalitech-logo-color.png")} alt="desalitech" title="desalitech"/>
+                        </Link>
+                    </div>
 
-                                <div>
-                                    <Link to="/app/app-module/forgot-password-1"
-                                          title="Reset Password"><IntlMessages id="appModule.forgotPassword"/></Link>
+                    <div className="login-form">
+                        <form>
+                            <fieldset>
+                                <TextField
+                                    id="email1"
+                                    label={<IntlMessages id="appModule.email"/>}
+                                    fullWidth
+                                    onChange={(event) => this.setState({email: event.target.value})}
+                                    defaultValue={email}
+                                    margin="normal"
+                                    className="mt-1"
+                                />
+                                <TextField
+                                    type="password"
+                                    id="password1"
+                                    label={<IntlMessages id="appModule.password"/>}
+                                    fullWidth
+                                    onChange={(event) => this.setState({password: event.target.value})}
+                                    defaultValue={password}
+                                    margin="normal"
+                                    className="mt-1"
+                                />
+                                <div className="mt-1 mb-2 d-flex justify-content-between align-items-center">
+                                    <FormControlLabel
+                                        control={
+                                            <Checkbox color="primary"
+                                                      value="gilad"
+                                            />
+                                        }
+                                        label={<IntlMessages id="appModule.rememberMe"/>}
+                                    />
+
+                                    <div>
+                                        <Link to="/app/app-module/forgot-password-2"
+                                              title="Reset Password"><IntlMessages
+                                            id="appModule.forgotPassword"/></Link>
+                                    </div>
                                 </div>
-                            </div>
 
-                            <Link to="/" className="btn jr-btn-rounded btn-primary btn-rounded"><IntlMessages
-                                id="appModule.signIn"/></Link>
+                                <Button color="primary" variant="contained" component={Link} to="/" className="text-white">
+                                    <IntlMessages id="appModule.signIn"/>
+                                </Button>
 
-                        </fieldset>
-                    </form>
+                            </fieldset>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
-};
+        );
+    }
+}
 
-export default Login1;
+export default Login;
