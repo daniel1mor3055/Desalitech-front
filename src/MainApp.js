ce2b9b44078309d6111353673b0a5dc43ff41c81
Jonathan Mor <mordajon@gmail.com>
2020-05-13 21:08:56 +0300

    Rvxy900l - Insert authentication integration (#13)

--- src/MainApp.js
+++ src/MainApp.js
@@ -1,22 +1,43 @@
-import React from 'react';
-import {ConnectedRouter, connectRouter} from 'connected-react-router'
-import {Provider} from 'react-redux';
-import {Route, Switch} from 'react-router-dom';
-import configureStore, {history} from './store';
+import React, { Component } from 'react';
+import { ConnectedRouter, connectRouter } from 'connected-react-router'
+import { Provider } from 'react-redux';
+import { Route, Switch } from 'react-router-dom';
+import configureStore, { history } from './store';
 import './firebase/firebase';
 import App from './containers/App';
-
+import config from './auth_config';
+import { Auth0Provider } from './Auth0Provider';
 
 export const store = configureStore();
 
-const MainApp = () =>
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <Switch>
-        <Route path="/" component={App}/>
-      </Switch>
-    </ConnectedRouter>
-  </Provider>;
+class MainApp extends Component {
 
+    onRedirectCallback = (appState) => {
+        history.push(
+            appState && appState.targetUrl
+                ? appState.targetUrl
+                : '/app/system-select'
+        );
+
+        console.log(appState);
+    };
+
+    render() {
+        return (<Provider store={store}>
+            <ConnectedRouter history={history}>
+                <Switch>
+                    <Auth0Provider
+                        domain={config.domain}
+                        client_id={config.clientId}
+                        redirect_uri={window.location.origin}
+                        audience={config.audience}
+                        onRedirectCallback={this.onRedirectCallback}>
+                        <Route path="/" component={App} />
+                    </Auth0Provider>
+                </Switch>
+            </ConnectedRouter>
+        </Provider>);
+    }
+}
 
 export default MainApp;
--- src/index.js
+++ src/index.js
@@ -5,22 +5,22 @@ const rootEl = document.getElementById('app-site');
 
 // Create a reusable render method that we can call more than once
 let render = () => {
-  // Dynamically import our main App component, and render it
-  const MainApp = require('./MainApp').default;
-  ReactDOM.render(
-    <MainApp/>,
-    rootEl
-  );
+    // Dynamically import our main App component, and render it
+    const MainApp = require('./MainApp').default;
+    ReactDOM.render(
+        <MainApp />,
+        rootEl
+    );
 };
 
 if (module.hot) {
-  module.hot.accept('./MainApp', () => {
-    const MainApp = require('./MainApp').default;
-    render(
-      <MainApp/>,
-      rootEl
-    );
-  });
+    module.hot.accept('./MainApp', () => {
+        const MainApp = require('./MainApp').default;
+        render(
+            <MainApp />,
+            rootEl
+        );
+    });
 }
 
 render();
