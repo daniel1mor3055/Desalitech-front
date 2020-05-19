ba7d60e27b72fc754f6bc3892564bcc357495cfc
Daniel Mor <dmor@infinidat.com>
2020-05-19 03:58:18 +0300

    Tags list table has changed, also alarms list table. Fixed issues with backed. Extra changes requested by jhon in previous pull requests.

--- src/api/tagsList.js
+++ src/api/tagsList.js
@@ -1,3 +1,29 @@
 import axios from 'axios';
+import {camelizeArrayOfObjects, camelizeObjectKeys, capitalizeObjectKeys} from "./utils";
 
-export const fetchTagsApi = (systemId) => (axios.get(`/system/tag-list?sysid=${systemId}`));
+export const fetchTagsApi = async (systemId) => {
+    try {
+        const response = await axios.get(`/system/tag-list?SysId=${systemId}`);
+        camelizeObjectKeys(response.data);
+        camelizeArrayOfObjects(response.data.tags);
+        return response.data;
+    } catch (err) {
+        console.log(err);
+        throw err;
+    }
+};
+
+export const postTagApi = async (systemId, tagData) => {
+    try {
+        const tagDataToPass = {
+            sysId: systemId,
+            ...tagData
+        };
+        capitalizeObjectKeys(tagDataToPass);
+        const response = await axios.post(`/system/tag-list`, tagDataToPass);
+        return response;
+    } catch (err) {
+        console.log(err);
+        throw err;
+    }
+};
---
+++ src/api/utils.js
@@ -0,0 +1,48 @@
+export const camelize = (str) => {
+    return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function (word, index) {
+        return index === 0 ? word.toLowerCase() : word.toUpperCase();
+    }).replace(/\s+/g, '');
+};
+
+export const camelizeObjectKeys = (object) => {
+    for (let property in object) {
+        if (object.hasOwnProperty(property)) {
+            const camelizedProperty = camelize(property);
+            if (property !== camelizedProperty) {
+                Object.defineProperty(object, camelizedProperty,
+                    Object.getOwnPropertyDescriptor(object, property));
+                delete object[property];
+            }
+        }
+    }
+};
+
+export const camelizeArrayOfObjects = (arrayOfObjects) => {
+    arrayOfObjects.forEach((object) => {
+        camelizeObjectKeys(object);
+    });
+};
+
+
+export const capitalizeFirstLetter = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+export const capitalizeObjectKeys = (object) => {
+    for (let property in object) {
+        if (object.hasOwnProperty(property)) {
+            const capitalizedProperty = capitalizeFirstLetter(property);
+            if (property !== capitalizedProperty) {
+                Object.defineProperty(object, capitalizedProperty,
+                    Object.getOwnPropertyDescriptor(object, property));
+                delete object[property];
+            }
+        }
+    }
+};
+
+export const capitalizeArrayOfObjectKeys = (arrayOfObjects) => {
+    arrayOfObjects.forEach((object) => {
+        capitalizeObjectKeys(object);
+    });
+};
